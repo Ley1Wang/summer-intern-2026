2dcd9ba0a7185222ee052f6b89a3150a7d5c1d2f
Ley1 Wang <wangleyi123456abcd@163.com>
2026-06-18 02:54:38 +0800

    w2

--- w2/request.py
+++ w2/request.py
@@ -1,30 +1,28 @@
 import requests
 from bs4 import BeautifulSoup
 from w1.sql import MySqlHelper
+
 url = "https://top.baidu.com/board"
 
 headers = {
-    "User-Agent":
-    "LeyiPhysicsBot/1.0 (leyiwang0624@gmail.com)"
+    "User-Agent": "Mozilla/5.0"
 }
 
 response = requests.get(url, headers=headers, timeout=10)
 response.raise_for_status()
-
-soup = BeautifulSoup(
-    response.text,
-    "html.parser"
-)
-
-
-article = soup.find(
-    "div",
-    class_="c-single-text-ellipsis"
-)
-if article:
-    print(article.get_text(strip=True))
-else:
-    print("没有找到目标内容")
-
+soup = BeautifulSoup(response.text, "html.parser")
 baidu_table = MySqlHelper()
-baidu_table.CREATE()
+baidu_table.CREATE("rank", "title", "desc", "hot")
+
+articles = soup.find_all("div", class_="c-single-text-ellipsis")
+print("articles数量:", len(articles))
+
+for rank, article in enumerate(articles, start=1):
+    title = article.get_text(strip=True)
+
+    if title:
+        baidu_table.INSERT(rank, title, "", "")
+
+baidu_table.SAVE("baidu_hot.json")
+
+print(baidu_table.SELECT())
