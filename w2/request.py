294586d36206dd0ed400b6dbd21b07f487c6a93f
Ley1 Wang <wangleyi123456abcd@163.com>
2026-06-18 03:34:23 +0800

    w2

--- w2/request.py
+++ w2/request.py
@@ -2,7 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 from w1.sql import MySqlHelper
 from urllib.parse import urljoin
-url = "https://top.baidu.com/board"
+url = "https://top.baidu.com/board?tab=realtime"
 
 headers = {
     "User-Agent": "Mozilla/5.0"
@@ -17,26 +17,26 @@ baidu_table = MySqlHelper()
 baidu_table.CREATE("title","url", "desc", "hot")
 
 articles = soup.find_all("div", class_="c-single-text-ellipsis")
-items = soup.select('div[class*="category-wrap_"]')
+print(articles[0].find_parent("a").prettify())
+items = soup.select('a[class*="title_"][href]')
 
 for item in items:
     article = item.select_one(".c-single-text-ellipsis")
-    desc_tag = item.select_one('[class*="hot-desc_"]')
-    hot_tag = item.select_one('[class*="hot-index_"]')
+
+    if article is None:
+        continue
+
     title = article.get_text(strip=True)
+    article_url = urljoin(url, item["href"])
+
+    container = item.find_parent("div")
+    desc_tag = container.select_one('[class*="hot-desc_"]')
+    hot_tag = container.select_one('[class*="hot-index_"]')
+
     desc = desc_tag.get_text(strip=True) if desc_tag else ""
     hot = hot_tag.get_text(strip=True) if hot_tag else ""
-    link = article.find_parent("a")
-    if link is None:
-        link = article.find("a")
 
-    if link and link.get("href"):
-        article_url = urljoin(url, link["href"])
-    else:
-        article_url = ""
-    if title:
-        baidu_table.INSERT(title,article_url, desc, hot)
-
+    baidu_table.INSERT(title, article_url, desc, hot)
 baidu_table.SAVE("baidu_hot.json")
 
 print(baidu_table.SELECT())
