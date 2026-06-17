d6cd4dd6c7b0ceb198dfbd04b8f6594eb3b6b42d
Ley1 Wang <wangleyi123456abcd@163.com>
2026-06-18 03:09:31 +0800

    w2

--- w2/request.py
+++ w2/request.py
@@ -1,7 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 from w1.sql import MySqlHelper
-
+from urllib.parse import urljoin
 url = "https://top.baidu.com/board"
 
 headers = {
@@ -14,16 +14,25 @@ response.raise_for_status()
 soup = BeautifulSoup(response.text, "html.parser")
 
 baidu_table = MySqlHelper()
-baidu_table.CREATE("title", "desc", "hot")
+baidu_table.CREATE("title","url", "desc", "hot")
 
 articles = soup.find_all("div", class_="c-single-text-ellipsis")
 print("articles数量:", len(articles))
 
-for rank, article in enumerate(articles, start=1):
+for desc_tag,hot_tag, article in enumerate(articles, start=1):
     title = article.get_text(strip=True)
+    desc = desc_tag.get_text(strip=True) if desc_tag else ""
+    hot = hot_tag.get_text(strip=True) if hot_tag else ""
+    link = article.find_parent("a")
+    if link is None:
+        link = article.find("a")
 
+    if link and link.get("href"):
+        article_url = urljoin(url, link["href"])
+    else:
+        article_url = ""
     if title:
-        baidu_table.INSERT(title, "", "")
+        baidu_table.INSERT(title,article_url, "", "")
 
 baidu_table.SAVE("baidu_hot.json")
 
